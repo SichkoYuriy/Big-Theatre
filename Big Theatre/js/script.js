d08c56a5b090cfff1eac8e9a2f8b6dcbc71fb5fc
SichkoYuriy <113180835+SichkoYuriy@users.noreply.github.com>
2022-09-15 16:09:31 +0300

    Add files via upload

--- Big Theatre/js/script.js
+++ Big Theatre/js/script.js
@@ -27,10 +27,74 @@ new Swiper('.image-slider1', {
 }); 
 
 new Swiper('.image-slider2', {
-    spaceBetween: 140,
-    slidesPerView: 4,
-    grid: {
-    rows: 2,
-    fill: 'row',
-  },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.1,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        },
+         375: {
+            slidesPerView: 1.3,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        },
+        425: {
+            slidesPerView: 1.15,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        },
+        475: {
+            slidesPerView: 1.3,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        },
+        576: {
+            slidesPerView: 1.8,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        },
+        768: {
+            slidesPerView: 2.5 ,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        },
+        1440: {
+            slidesPerView: 2.5,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        },
+        1919: {
+            slidesPerView: 4,
+            grid: {
+            rows: 2,
+            fill: 'row',
+            },
+        }
+    },
 }); 
+
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(window).scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 100) {
+            $(".header").css('background-color', 'rgba(51, 51, 51)');
+        } else {
+            $(".header").css('background-color', 'rgba(51, 51, 51,0)');
+        }
+    });
+});
